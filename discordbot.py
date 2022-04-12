b2abf9dd0e026cf7e0a45bae524d318099a0cde0
Shadblade5 <shadblade8@gmail.com>
2022-04-12 14:22:49 -0400

    DB added

---
+++ discordbot.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import json
+
+with open('config.json') as f:
+    data = json.load(f)
+    token = data["TOKEN"]
+    prefix = data["PREFIX"]
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=prefix, description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(token)
+
+"""
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(token)
+"""
